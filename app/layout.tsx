664cc63688ace59597871d7cc69f290de3146d24
vaishnaviagyarapu <aagyarapuvaishavi@gmail.com>
2026-03-14 14:24:20 +0530

    fix api route

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,19 @@
 
 import "./globals.css"
 import Navbar from "@/components/Navbar"
+import { ReactNode } from "react"
 
 export const metadata = {
   title: "Learning Streak Tracker",
   description: "Track your daily learning progress",
 }
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body>
-
         <Navbar />
-
         {children}
-
       </body>
     </html>
   )
